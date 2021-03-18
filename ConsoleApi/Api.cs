17624947c20fba97588ffeca63449f58f9b2a883
Toyro98 <teddymolin1@gmail.com>
2021-03-18 22:06:13 +0100

    Update Api.cs
    Removed unused stuff

--- ConsoleApi/Api.cs
+++ ConsoleApi/Api.cs
@@ -1,58 +1,38 @@
-﻿using System;
-
-namespace Api
+﻿namespace Api
 {
-    class RunInfo
+    class Run
     {
         public Data[] data { get; set; }
 
         public class Data
         {
             public string Id { get; set; }
-            public string Weblink { get; set; }
-            public string Game { get; set; }
             public string Level { get; set; }
             public string Category { get; set; }
-            public object Comment { get; set; }
-            public Status Status { get; set; }
             public Player[] Players { get; set; }
             public string Date { get; set; }
-            public DateTime Submitted { get; set; }
             public Times Times { get; set; }
         }
-        public class Status
-        {
-            public string status { get; set; }
-            public string Examiner { get; set; }
-            public DateTime Verifydate { get; set; }
-        }
+
         public class Times
         {
-            public string Primary { get; set; }
             public float Primary_t { get; set; }
-            public string Realtime { get; set; }
-            public float Realtime_t { get; set; }
-            public object Realtime_noloads { get; set; }
-            public int Realtime_noloads_t { get; set; }
-            public object Ingame { get; set; }
-            public int Ingame_t { get; set; }
         }
 
         public class Player
         {
             public string Rel { get; set; }
             public string Id { get; set; }
-            public string Uri { get; set; }
+            public string Name { get; set; }
         }
     }
 
-    class LeaderboardInfo
+    class Leaderboard
     {
         public Data[] data { get; set; }
 
         public class Data
         {
-            public string Id { get; set; }
             public Names names { get; set; }
             public int Place { get; set; }
             public Run run { get; set; }
@@ -61,24 +41,22 @@ namespace Api
         public class Names
         {
             public string International { get; set; }
-            public object Japanese { get; set; }
         }
 
         public class Run
         {
             public string Id { get; set; }
-            public string Level { get; set; }
-            public string Category { get; set; }
         }
     }
 
-    class CategoryInfo
+    class Category
     {
         public Data data { get; set; }
 
         public class Data
         {
             public Categories Categories { get; set; }
+            public Levels Levels { get; set; }
         }
 
         public class Categories
@@ -91,22 +69,31 @@ namespace Api
             public string Id { get; set; }
             public string Name { get; set; }
         }
+
+        public class Levels
+        {
+            public Datum2[] Data { get; set; }
+        }
+
+        public class Datum2
+        {
+            public string Id { get; set; }
+            public string Name { get; set; }
+        }
     }
 
-    class UserInfo
+    class User
     {
         public Data data { get; set; }
 
         public class Data
         {
-            public string Id { get; set; }
             public Names names { get; set; }
         }
 
         public class Names
         {
             public string International { get; set; }
-            public object Japanese { get; set; }
         }
     }
 }
