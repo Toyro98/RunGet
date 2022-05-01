7668d448fcc28b02e7375c525f7ffc52796b29de
Toyro98 <teddymolin1@gmail.com>
2022-05-01 23:28:52 +0200

    Update Program.cs
    Catch errors and write it to the console

--- RunGet/src/console/Program.cs
+++ RunGet/src/console/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Threading;
+using Pastel;
 
 namespace RunGet
 {
@@ -7,12 +8,28 @@ namespace RunGet
     {
         static void Main(string[] args) 
         {
+            // Get the latests run id from the API
             Runs.GetLatestRunId();
-            
+
+            // Infinite loop
             while (true)
             {
+                // Sleep for 5 min
                 Thread.Sleep(TimeSpan.FromMinutes(5));
-                Runs.LookForNewRuns();
+
+                // Catch any errors and write it to the console
+                try
+                {
+                    Runs.LookForNewRuns();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("".PadRight(112, '='));
+                    Console.WriteLine("[{0}] {1}\n", DateTime.Now.ToString().Pastel("#808080"), "Error!! :(".Pastel("#8C0000"));
+                    Console.WriteLine(ex.ToString());
+                    Console.ReadLine();
+                    Environment.Exit(0);
+                }
             }
         }
     }
