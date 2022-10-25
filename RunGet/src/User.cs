d860483f8a058e7cd96fb1d01b4ff60f40b681e4
Toyro98 <teddymolin1@gmail.com>
2022-10-25 20:56:30 +0200

    Update User.cs
    Changed so you don't have to go through run.Players to get the data

--- RunGet/src/User.cs
+++ RunGet/src/User.cs
@@ -2,43 +2,42 @@
 {
     public class User
     {
-        public static string GetNames(RunsApi.Data run)
+        public static string GetNames(RunsApi.Players run)
         {
-            // Check if the run only has one player
-            if (run.Players.Data.Length == 1)
+            // Return the name if it's only one runner
+            if (run.Data.Length == 1)
             {
-                if (run.Players.Data[0].Rel == "user")
+                if (run.Data[0].Rel == "user")
                 {
-                    return run.Players.Data[0].Names.International;
+                    return run.Data[0].Names.International;
                 }
 
-                return run.Players.Data[0].Name;
+                return run.Data[0].Name;
             }
 
-            // The run has multiple runners. We only want to display the first 2 names
+            // The run has multiple runners. Only display the first 2 names
             string[] players = new string[2];
 
             for (int i = 0; i < players.Length; i++)
             {
-                if (run.Players.Data[i].Rel == "user")
+                if (run.Data[i].Rel == "user")
                 {
-                    players[i] = run.Players.Data[i].Names.International;
+                    players[i] = run.Data[i].Names.International;
                 }
                 else
                 {
-                    players[i] = run.Players.Data[i].Name;
+                    players[i] = run.Data[i].Name;
                 }
             }
 
-            if (run.Players.Data.Length == 2)
+            if (run.Data.Length == 2)
             {
                 return players[0] + " and " + players[1];
             }
-            
+
             // Instead of displaying everyones names in the embed title
             // It will show the first two players and say "x, y, and z more runner(s)"
-            int amountOfRunners = run.Players.Data.Length - 2;
-            return players[0] + ", " + players[1] + ", and " + amountOfRunners + " other runner" + ((amountOfRunners == 1) ? "" : "s");
+            return players[0] + ", " + players[1] + ", and " + (run.Data.Length - 2) + " other runner" + ((run.Data.Length - 2) == 1 ? "" : "s");
         }
     }
 }
