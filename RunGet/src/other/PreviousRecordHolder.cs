e3e2940cba38a9ef082d20ca30a7baa1198a2852
Toyro98 <teddymolin1@gmail.com>
2022-11-01 23:08:03 +0100

    Create PreviousRecordHolder.cs

---
+++ RunGet/src/other/PreviousRecordHolder.cs
@@ -0,0 +1,14 @@
+﻿namespace RunGet
+{
+    public class PreviousRecordHolder
+    {
+        public int days;
+        public string names;
+
+        public PreviousRecordHolder()
+        {
+            days = -1;
+            names = null;
+        }
+    }
+}
