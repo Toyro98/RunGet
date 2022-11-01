f9d02d6c41e9683e706c1be981384bdb5cff7a80
Toyro98 <teddymolin1@gmail.com>
2022-11-01 23:07:54 +0100

    Create Speedrun.cs

---
+++ RunGet/src/other/Speedrun.cs
@@ -0,0 +1,11 @@
+﻿namespace RunGet
+{
+    public class Speedrun
+    {
+        public string Id { get; set; }
+        public string LatestRunId { get; set; }
+        public string WebhookUrl { get; set; }
+        public string DefaultPlatform { get; set; }
+        public RunsModel.Data[] Runs { get; set; }
+    }
+}
