1c3f24f431b2d650b0f15694c40d932e872f1d8c
Toyro98 <teddymolin1@gmail.com>
2022-11-30 18:56:57 +0100

    Update PreviousRecordHolder.cs
    Added first and second date so it can get the days difference

--- RunGet/src/other/PreviousRecordHolder.cs
+++ RunGet/src/other/PreviousRecordHolder.cs
@@ -1,14 +1,25 @@
-﻿namespace RunGet
+﻿using System;
+
+namespace RunGet
 {
     public class PreviousRecordHolder
     {
         public int days;
         public string names;
 
+        public DateTime? firstPlaceDate;
+        public DateTime? secondPlaceDate;
+
         public PreviousRecordHolder()
         {
             days = -1;
             names = null;
+        }
+
+        public PreviousRecordHolder(DateTime? firstPlaceDate, DateTime? secondPlaceDate)
+        {
+            this.firstPlaceDate = firstPlaceDate;
+            this.secondPlaceDate = secondPlaceDate;
         }
     }
 }
