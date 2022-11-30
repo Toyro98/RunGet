b7ba2d387d2dc0464d0015233741b3382ef6661e
Toyro98 <teddymolin1@gmail.com>
2022-11-30 18:58:54 +0100

    Update Date.cs
    Use NodaTime to get accurate time between two dates

--- RunGet/src/Date.cs
+++ RunGet/src/Date.cs
@@ -1,28 +1,85 @@
 ﻿using System;
+using System.Collections.Generic;
+using NodaTime;
 
 namespace RunGet
 {
     public static class Date
     {
+        private const string Year = "year";
+        private const string Month = "month";
+        private const string Day = "day";
+
         public static string FormatDate(DateTime date)
         {
             return string.Format("{0} {1:MMM yyyy}", date.Day, date);
         }
 
-        public static int GetDifferenceInDays(LeaderboardModel.Root leaderboard, RunsModelLight.Root personalBests)
+        public static string FormatDate(DateTime startDate, DateTime endDate)
+        {
+            if (startDate == endDate)
+            {
+                return "less than a day";
+            }
+
+            if (startDate > endDate)
+            {
+                (endDate, startDate) = (startDate, endDate);
+            }
+
+            LocalDate start = new LocalDate(startDate.Year, startDate.Month, startDate.Day);
+            LocalDate end = new LocalDate(endDate.Year, endDate.Month, endDate.Day);
+            Period period = Period.Between(start, end);
+
+            Dictionary<string, int> keyValues = new Dictionary<string, int>
+            {
+                { Year, period.Years },
+                { Month, period.Months },
+                { Day, period.Days }
+            };
+
+            string date = "";
+
+            foreach (var item in keyValues)
+            {
+                if (item.Value == 0)
+                {
+                    continue;
+                }
+
+                date += item.Value + " " + (item.Value < 2 ? item.Key : item.Key + "s") + ", ";
+            }
+
+            if (date.EndsWith(", "))
+            {
+                date = date[..^2];
+            }
+
+            int a = date.LastIndexOf(",");
+
+            if (a != -1)
+            {
+                date = date.Remove(a, 2).Insert(a, " and ");
+            }
+
+            return date;
+        }
+
+        public static PreviousRecordHolder GetDifferenceInDays(LeaderboardModel.Root leaderboard, RunsModelLight.Root personalBests)
         {
             if (leaderboard.Data.Runs.Length < 2 && personalBests.Data.Length < 2)
             {
-                return int.MinValue;
+                return new();
             }
 
             var currentWorldRecordDate = leaderboard.Data.Runs[0].Run.Date ?? DateTime.MaxValue;
             var previousWorldRecordDate = leaderboard.Data.Runs[1].Run.Date ?? DateTime.MaxValue;
-            var previousPersonalBestDate = DateTime.MaxValue;
 
-            float currentWorldRecordTime = leaderboard.Data.Runs[0].Run.Times.Primary_t;
-            float previousWorldRecordTime = leaderboard.Data.Runs[1].Run.Times.Primary_t;
-            float previousPersonalBestTime = float.MaxValue;
+            var currentWorldRecordTime = leaderboard.Data.Runs[0].Run.Times.Primary_t;
+            var previousWorldRecordTime = leaderboard.Data.Runs[1].Run.Times.Primary_t;
+            
+            var previousPersonalBestDate = DateTime.MaxValue;
+            var previousPersonalBestTime = float.MaxValue;
 
             for (int i = 0; i < personalBests.Data.Length; i++)
             {
@@ -31,28 +88,30 @@ namespace RunGet
                     continue;
                 }
 
-                if (previousWorldRecordTime > personalBests.Data[i].Times.Primary_t)
+                if (currentWorldRecordTime == personalBests.Data[i].Times.Primary_t)
                 {
-                    if (currentWorldRecordTime != personalBests.Data[i].Times.Primary_t)
-                    {
-                        previousPersonalBestTime = personalBests.Data[i].Times.Primary_t;
-                        previousPersonalBestDate = (DateTime)personalBests.Data[i].Date;
-                        break;
-                    }
+                    return new(previousWorldRecordDate.Date, currentWorldRecordDate.Date);
+                }
+
+                if (currentWorldRecordTime > personalBests.Data[i].Times.Primary_t)
+                {
+                    previousPersonalBestTime = personalBests.Data[i].Times.Primary_t;
+                    previousPersonalBestDate = (DateTime)personalBests.Data[i].Date;
+                    break;
                 }
             }
 
             if (previousPersonalBestDate == DateTime.MaxValue)
             {
-                return (currentWorldRecordDate.Date - previousWorldRecordDate.Date).Days;
+                return new(personalBests.Data[0].Date, personalBests.Data[1].Date);
             }
 
             if (previousWorldRecordDate > previousPersonalBestDate || previousWorldRecordTime > previousPersonalBestTime)
             {
-                return (currentWorldRecordDate.Date - previousPersonalBestDate.Date).Days;
+                return new(currentWorldRecordDate.Date, previousPersonalBestDate.Date);
             }
 
-            return int.MinValue;
+            return new();
         }
     }
 }
